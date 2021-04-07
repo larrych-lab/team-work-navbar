9a6d246762697c2bbb5f28a3862288b1ca84cd1f
larrych <larrych@adminnoMacBook-Air.local>
2021-04-08 08:52:06 +0900

    raw code

---
+++ create-delete-function.js
@@ -0,0 +1,7 @@
+function toggleActive() {
+  $(document).on("click", ".nav-bar__item", function () {
+    $(".nav-bar__item.active").removeClass("active");
+    $(this).toggleClass("active");
+  });
+}
+toggleActive();
